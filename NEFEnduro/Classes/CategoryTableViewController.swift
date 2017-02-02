b19bbdc5272dce6d6626d89890935cb42a9ff708
Chaitanya <chaitanyapandit@me.com>
2017-02-02 23:35:21 +0530

    Added HF card Layout

--- NEFEnduro/Classes/CategoryTableViewController.swift
+++ NEFEnduro/Classes/CategoryTableViewController.swift
@@ -10,24 +10,40 @@ import UIKit
 import SnapKit
 import TransitionTreasury
 import TransitionAnimation
+import HFCardCollectionViewLayout
 
-class CategoryTableViewController: UIViewController, NavgationTransitionable{
+class CategoryTableViewController:  UICollectionViewController, HFCardCollectionViewLayoutDelegate, NavgationTransitionable {
     
+    //Card Layout
+    var cardCollectionViewLayout: HFCardCollectionViewLayout?
+
     /// Transiton delegate
     var tr_pushTransition: TRNavgationTransitionDelegate?
-
+    
+    var colorArray = [UIColor]()
+    
+    var shouldSetupBackgroundView = true
     var type:String? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.colorArray.insert(UIColor.red, at: 0)
+        self.colorArray.insert(UIColor.blue, at: 1)
+        self.colorArray.insert(UIColor.yellow, at: 2)
+
         navigationController?.isNavigationBarHidden = false
+        self.collectionView?.register(ExampleCollectionViewCell.self, forCellWithReuseIdentifier: "CardCell")
+        
+        if let cardCollectionViewLayout = self.collectionView?.collectionViewLayout as? HFCardCollectionViewLayout {
+            self.cardCollectionViewLayout = cardCollectionViewLayout
+        }
         
         view.backgroundColor = UIColor.red
         
         let button = UIButton()
         view.addSubview(button)
-        button.setTitle("POP", for: .normal)
+        button.setTitle("Back", for: .normal)
         button.addTarget(self, action: #selector(self.popButtonTapped(button:)), for: .touchUpInside)
         button.snp.makeConstraints { (make) in
             make.center.equalTo(view)
@@ -35,6 +51,49 @@ class CategoryTableViewController: UIViewController, NavgationTransitionable{
         }
     }
 
+    // MARK: CollectionView
+    
+    func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, canUnrevealCardAtIndex index: Int) -> Bool {
+        if(self.colorArray.count == 1) {
+            return false
+        }
+        return true
+    }
+    
+    func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, willRevealCardAtIndex index: Int) {
+        if let cell = self.collectionView?.cellForItem(at: IndexPath(item: index, section: 0)) as? ExampleCollectionViewCell {
+            cell.cardCollectionViewLayout = self.cardCollectionViewLayout
+            cell.cardIsRevealed(true)
+        }
+    }
+    
+    func cardCollectionViewLayout(_ collectionViewLayout: HFCardCollectionViewLayout, willUnrevealCardAtIndex index: Int) {
+        if let cell = self.collectionView?.cellForItem(at: IndexPath(item: index, section: 0)) as? ExampleCollectionViewCell {
+            cell.cardCollectionViewLayout = self.cardCollectionViewLayout
+            cell.cardIsRevealed(false)
+        }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.colorArray.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! ExampleCollectionViewCell
+        cell.backgroundColor = self.colorArray[indexPath.item]
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.cardCollectionViewLayout?.revealCardAt(index: indexPath.item)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let tempColor = self.colorArray[sourceIndexPath.item]
+        self.colorArray.remove(at: sourceIndexPath.item)
+        self.colorArray.insert(tempColor, at: destinationIndexPath.item)
+    }
+    
     func popButtonTapped(button : UIButton) -> Void {
         navigationController?.popViewController(animated: true)
     }
---
+++ NEFEnduro/Classes/ExampleCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  ExampleCollectionViewCell.swift
+//  HFCardCollectionViewLayoutExample
+//
+//  Created by Hendrik Frahmann on 02.11.16.
+//  Copyright © 2016 Hendrik Frahmann. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import HFCardCollectionViewLayout
+
+class ExampleCollectionViewCell: HFCardCollectionViewCell {
+    
+    var cardCollectionViewLayout: HFCardCollectionViewLayout?
+    
+    @IBOutlet var buttonFlip: UIButton?
+    @IBOutlet var tableView: UITableView?
+    @IBOutlet var labelText: UILabel?
+    
+    @IBOutlet var backView: UIView?
+    @IBOutlet var buttonFlipBack: UIButton?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.buttonFlip?.isHidden = true
+        self.tableView?.scrollsToTop = false
+    }
+    
+    func cardIsRevealed(_ isRevealed: Bool) {
+        self.buttonFlip?.isHidden = !isRevealed
+        self.tableView?.scrollsToTop = isRevealed
+    }
+    
+    @IBAction func buttonFlipAction() {
+        if let backView = self.backView {
+            // Same Corner radius like the contentview of the HFCardCollectionViewCell
+            backView.layer.cornerRadius = self.cornerRadius
+            backView.layer.masksToBounds = true
+            
+            self.cardCollectionViewLayout?.flipRevealedCard(toView: backView)
+        }
+    }
+    
+    
+}
